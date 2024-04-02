67fde155d86afb917902cbcc0faf79b8de47bd21
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-04-02 16:05:38 +0200

    [ADDED] display background image for home_page

--- gui/menu.py
+++ gui/menu.py
@@ -1,11 +1,12 @@
 from gui.element import Element
 import pygame
+
 class Menu(Element):
     def __init__(self):
-        Element.__init__(self)
+        super().__init__()
         self.home_page = True
         self.menu_page = False
-    
+
     def home(self):
         self.menu_run = True
         while self.menu_run:
@@ -13,10 +14,15 @@ class Menu(Element):
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                                    
+
                 # Event keyboard
-                elif event.type == pygame.KEYDOWN:
-                    pass
-            
+                elif event.type == pygame.KEYDOWN and self.home_page:
+                    pygame.quit()
+
+            if self.home_page:
+                self.img(350, 350, 1400, 750, "menu/background_home1.jpg")
+
+            if self.menu_page:
+                pass
+
             self.update()
-        
