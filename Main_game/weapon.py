1d4dad516a68e22bc2b9716705ba6688149c6023
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-04-04 15:25:08 +0200

    collision problem resolved

--- Main_game/weapon.py
+++ Main_game/weapon.py
@@ -2,6 +2,7 @@ import pygame as pg
 from Main_game.Setting import *
 
 
+# Dans weapon.py
 class Weapon(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -22,31 +23,17 @@ class Weapon(pg.sprite.Sprite):
     def shoot(self, bullets_group):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         bullets_group.add(bullet)
-    
 
-    def move(self):
-        keys = pg.key.get_pressed()
 
-        # Check left movement
-        if keys[pg.K_q] and self.rect.left > 0:
-            self.rect.x -= 5
-        # Check right movement
-        if keys[pg.K_d] and self.rect.right < WIDTH:
-            self.rect.x += 5
-
-    def collide(self):
-        if self.rect.left < 0 or self.rect.right > WIDTH:
-            return True
-        return False
+# Dans bullet.py
 class Bullet(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pg.Surface((10, 10))  # Example surface for bullet image
-        self.image.fill((0, 255, 0))  # Red circle as placeholder
+        self.image.fill((255, 0, 0))  # Red circle as placeholder
         self.rect = self.image.get_rect(center=(x, y))
         self.speed = -10  # Bullet speed
-        
-        
+
     def update(self):
         self.rect.y += self.speed
         if self.rect.bottom < 0:
