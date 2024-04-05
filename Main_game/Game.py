1d59a46e28b029c920d94b713b77f66e7156ff96
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-04-05 12:52:36 +0200

    new children on class game

--- Main_game/Game.py
+++ Main_game/Game.py
@@ -1,10 +1,12 @@
 import pygame as pg
 from Main_game.Setting import *
 from Main_game.weapon import Weapon, Bullet
+from gui.element import Element
 
 
-class Game:
+class Game(Element):
     def __init__(self):
+        Element.__init__(self)
         self.running = True
         self.display = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
@@ -26,7 +28,7 @@ class Game:
                         self.weapon.shoot(self.all_bullets)  # Pass all_bullets group to shoot method
                         self.last_shot_time = pg.time.get_ticks()  # Update the last shot time
 
-    def update(self):
+    def update_shooter(self):
         self.weapon.move()  # Move the weapon
         for bullet in self.all_bullets:
             bullet.update()  # Update each bullet's position
@@ -35,11 +37,11 @@ class Game:
         self.display.fill((0, 0, 0))  # Fill the screen with black color
         self.display.blit(self.weapon.image, self.weapon.rect)  # Draw the weapon's image
         self.all_bullets.draw(self.display)  # Draw all bullets
-        pg.display.flip()  # Update the display
+        self.update()  # Update the display
 
     def run(self):
         while self.running:
             self.handle_events()
-            self.update()
+            self.update_shooter()
             self.draw()
             self.clock.tick(FPS)
