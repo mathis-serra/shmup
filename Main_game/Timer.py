64b59ca84ffbae7ac5ba8927b959855aa2b70abf
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-04-11 23:32:14 +0200

    timer is finish

--- Main_game/Timer.py
+++ Main_game/Timer.py
@@ -2,15 +2,17 @@ import pygame
 
 class Timer:
     def __init__(self):
+        self.clock = pygame.time.Clock()
         self.time_passed = 0
-        self.timer_event = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.timer_event, 1000)  # Timer événement toutes les secondes
+        self.last_tick = pygame.time.get_ticks()
 
     def update(self):
-        self.time_passed += 1
+        current_tick = pygame.time.get_ticks()
+        self.time_passed += current_tick - self.last_tick
+        self.last_tick = current_tick
 
     def get_time(self):
-        hours = self.time_passed // 3600
-        minutes = (self.time_passed % 3600) // 60
-        seconds = self.time_passed % 60
+        seconds = self.time_passed // 1000
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
         return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
